3785b1e5ef2db03eab3fb4245d44337905aa96a1
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-03-17 16:05:46 +0900

    1605

---
+++ study_C/study_C/main0317-10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+typedef struct vision {
+	double left;
+	double right;
+}Vision;
+
+Vision exchange(Vision);
+
+int main() 
+{
+	Vision rot;
+	printf("ÁÂ, ¿ì ½Ã·Â ÀÔ·Â>> (ex: 1.2 1.5)\n");
+	scanf_s("%lf %lf", &rot.left, &rot.right);
+
+	rot = exchange(rot);
+
+	printf("¹Ù²ï ½Ã·Â : %.1lf, %.1lf\n", rot.left, rot.right);
+	return 0;
+}
+
+Vision exchange(Vision v) {
+	double temp = v.left;
+	v.left = v.right;
+	v.right = temp;
+	return v;
+}
