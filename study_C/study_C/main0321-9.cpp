d1ddb3fbe209e84cc6773f0289ebaf3c926cdf11
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-03-21 14:42:55 +0900

    1442

--- study_C/study_C/main0321-9.cpp
+++ study_C/study_C/main0321-9.cpp
@@ -12,29 +12,37 @@ private:
 	char* name;
 	int age;
 public:
-	void showInfo() 
+	void showInfo()
 	{
 		std::cout << "name : " << name << " age : " << age << std::endl;
 	};
 public:
-	Human(const char* _name = nullptr, int _age = 0)
-		: name(new char[strlen(_name)+1])
+	Human() { std::cout << "¾øÀ»¹«" << std::endl; }
+	Human(const char* _name, int _age)
+		: name(new char[strlen(_name) + 1])
 		, age(_age)
 	{
 		std::cout << "cons" << std::endl;
 		strcpy_s(name, strlen(_name) + 1, _name);
 	}
 	Human(const Human& _other)
-		: name(new char[strlen(_other.name)+1])
+		: name(new char[strlen(_other.name) + 1])
 		, age(_other.age)
 	{
 		std::cout << "copy" << std::endl;
-		strcpy_s(name, strlen(_other.name)+1, _other.name);
+		strcpy_s(name, strlen(_other.name) + 1, _other.name);
 	}
 	~Human()
 	{
 		std::cout << "dest" << std::endl;
 		delete[] name;
+	}
+	Human& operator = (const Human& _other)
+	{
+		name = new char[strlen(_other.name) + 1];
+		strcpy_s(name, strlen(_other.name) + 1, _other.name);
+		age = _other.age;
+		return *this;
 	}
 };
 
